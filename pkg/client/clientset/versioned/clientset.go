4d7fdcd7c6411eba95da35e39b4cf3688f7ea949
Piyush Nimbalkar <piyush@portworx.com>
2020-10-07 17:12:54 -0700

    Revert "Delete the v1alpha1 API objects and client"
    This reverts commit 3084c3742aeace90f5e8eabce0c9c2774dfe4fe2.

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -20,6 +20,7 @@ package versioned
 
 import (
 	corev1 "github.com/libopenstorage/operator/pkg/client/clientset/versioned/typed/core/v1"
+	corev1alpha1 "github.com/libopenstorage/operator/pkg/client/clientset/versioned/typed/core/v1alpha1"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
 	flowcontrol "k8s.io/client-go/util/flowcontrol"
@@ -27,6 +28,7 @@ import (
 
 type Interface interface {
 	Discovery() discovery.DiscoveryInterface
+	CoreV1alpha1() corev1alpha1.CoreV1alpha1Interface
 	CoreV1() corev1.CoreV1Interface
 }
 
@@ -34,7 +36,13 @@ type Interface interface {
 // version included in a Clientset.
 type Clientset struct {
 	*discovery.DiscoveryClient
-	coreV1 *corev1.CoreV1Client
+	coreV1alpha1 *corev1alpha1.CoreV1alpha1Client
+	coreV1       *corev1.CoreV1Client
+}
+
+// CoreV1alpha1 retrieves the CoreV1alpha1Client
+func (c *Clientset) CoreV1alpha1() corev1alpha1.CoreV1alpha1Interface {
+	return c.coreV1alpha1
 }
 
 // CoreV1 retrieves the CoreV1Client
@@ -58,6 +66,10 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 	}
 	var cs Clientset
 	var err error
+	cs.coreV1alpha1, err = corev1alpha1.NewForConfig(&configShallowCopy)
+	if err != nil {
+		return nil, err
+	}
 	cs.coreV1, err = corev1.NewForConfig(&configShallowCopy)
 	if err != nil {
 		return nil, err
@@ -74,6 +86,7 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 // panics if there is an error in the config.
 func NewForConfigOrDie(c *rest.Config) *Clientset {
 	var cs Clientset
+	cs.coreV1alpha1 = corev1alpha1.NewForConfigOrDie(c)
 	cs.coreV1 = corev1.NewForConfigOrDie(c)
 
 	cs.DiscoveryClient = discovery.NewDiscoveryClientForConfigOrDie(c)
@@ -83,6 +96,7 @@ func NewForConfigOrDie(c *rest.Config) *Clientset {
 // New creates a new Clientset for the given RESTClient.
 func New(c rest.Interface) *Clientset {
 	var cs Clientset
+	cs.coreV1alpha1 = corev1alpha1.New(c)
 	cs.coreV1 = corev1.New(c)
 
 	cs.DiscoveryClient = discovery.NewDiscoveryClient(c)
--- pkg/client/informers/externalversions/generic.go
+++ pkg/client/informers/externalversions/generic.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 
 	v1 "github.com/libopenstorage/operator/pkg/apis/core/v1"
+	v1alpha1 "github.com/libopenstorage/operator/pkg/apis/core/v1alpha1"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
 )
@@ -57,6 +58,12 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1().StorageClusters().Informer()}, nil
 	case v1.SchemeGroupVersion.WithResource("storagenodes"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1().StorageNodes().Informer()}, nil
+
+		// Group=core.libopenstorage.org, Version=v1alpha1
+	case v1alpha1.SchemeGroupVersion.WithResource("storageclusters"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().StorageClusters().Informer()}, nil
+	case v1alpha1.SchemeGroupVersion.WithResource("storagenodes"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().StorageNodes().Informer()}, nil
 
 	}
 
